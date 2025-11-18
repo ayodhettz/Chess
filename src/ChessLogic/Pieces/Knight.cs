6e52777a01a159a164773958d43c13d9734bee72
Ayod Hettiarachchi <ayodhettiarachchi@gmail.com>
2025-11-18 17:32:00 +0530

    Update Knight.cs

--- src/ChessLogic/Pieces/Knight.cs
+++ src/ChessLogic/Pieces/Knight.cs
@@ -18,5 +18,27 @@ namespace ChessLogic
         {
             return new Knight(Colour) { HasMoved = this.HasMoved };
         }
+
+        private static IEnumerable<Position> PotentialToPositions(Position from)
+        {
+            foreach(Direction vertical in new Direction[] { Direction.North, Direction.South })
+            {
+                foreach(Direction horizontal in new Direction[] { Direction.East, Direction.West })
+                {
+                    yield return from + vertical + vertical + horizontal;
+                    yield return from + vertical + horizontal + horizontal;
+                }
+            }
+        }
+
+        private IEnumerable<Position> ValidMovePositions(Position from, Board board)
+        {
+            return PotentialToPositions(from).Where(pos => Board.IsInside(pos) && (board.IsEmpty(pos) || board[pos].Colour != Colour));
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return ValidMovePositions(from, board).Select(to => new NormalMove(from, to));
+        }
     }
 }
