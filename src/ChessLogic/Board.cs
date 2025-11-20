7d7f48f60cae8f1390a706a5801c1a99233c4e5d
Ayod Hettiarachchi <ayodhettiarachchi@gmail.com>
2025-11-20 14:34:18 +0530

    Update Board.cs

--- src/ChessLogic/Board.cs
+++ src/ChessLogic/Board.cs
@@ -32,27 +32,27 @@ namespace ChessLogic
 
         private void AddStartingPieces()
         {
-            this[0,0] = new Rook(Player.Black);
-            this[0,1] = new Knight(Player.Black);
-            this[0,2] = new Bishop(Player.Black);
-            this[0,3] = new Queen(Player.Black);
-            this[0,4] = new King(Player.Black);
-            this[0,5] = new Bishop(Player.Black);
-            this[0,6] = new Knight(Player.Black);
-            this[0,7] = new Rook(Player.Black);
-            for(int col = 0; col < 8; col++)
+            this[0, 0] = new Rook(Player.Black);
+            this[0, 1] = new Knight(Player.Black);
+            this[0, 2] = new Bishop(Player.Black);
+            this[0, 3] = new Queen(Player.Black);
+            this[0, 4] = new King(Player.Black);
+            this[0, 5] = new Bishop(Player.Black);
+            this[0, 6] = new Knight(Player.Black);
+            this[0, 7] = new Rook(Player.Black);
+            for (int col = 0; col < 8; col++)
             {
                 this[1, col] = new Pawn(Player.Black);
                 this[6, col] = new Pawn(Player.White);
             }
-            this[7,0] = new Rook(Player.White);
-            this[7,1] = new Knight(Player.White);
-            this[7,2] = new Bishop(Player.White);
-            this[7,3] = new Queen(Player.White);
-            this[7,4] = new King(Player.White);
-            this[7,5] = new Bishop(Player.White);
-            this[7,6] = new Knight(Player.White);
-            this[7,7] = new Rook(Player.White);
+            this[7, 0] = new Rook(Player.White);
+            this[7, 1] = new Knight(Player.White);
+            this[7, 2] = new Bishop(Player.White);
+            this[7, 3] = new Queen(Player.White);
+            this[7, 4] = new King(Player.White);
+            this[7, 5] = new Bishop(Player.White);
+            this[7, 6] = new Knight(Player.White);
+            this[7, 7] = new Rook(Player.White);
         }
 
         public static bool IsInside(Position pos)
@@ -63,6 +63,34 @@ namespace ChessLogic
         public bool IsEmpty(Position pos)
         {
             return this[pos] == null;
+        }
+
+        public IEnumerable<Position> AllPositions()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    Position pos = new Position(row, col);
+
+                    if (!IsEmpty(pos))
+                    {
+                        yield return pos;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Position> PositionsOfPlayer(Player player)
+        {
+            return AllPositions().Where(pos => this[pos].Colour == player);
+        }
+
+        public bool IsInCheck(Player player)
+        {
+            return AllPositions()
+                .Where(pos => this[pos].Colour != player)
+                .Any(pos => this[pos].CanCaptureOpponentKing(pos, this));
         }
     }
 }
